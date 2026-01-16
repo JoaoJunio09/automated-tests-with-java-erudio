9cabb057e2a6a0889c35b4eae901705a8f0495ee
joao_junio <joaojunio818@gmail.com>
2026-01-16 16:26:31 -0300

    Initial Commit - Automated-tests-with-java-erudio

---
+++ src/test/java/br/com/erudio/automated_tests_with_java_erudio/FooBarTest.java
@@ -0,0 +1,11 @@
+package br.com.erudio.automated_tests_with_java_erudio;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+    @Test
+    void test() {
+
+    }
+}
---
+++ src/test/java/br/com/erudio/automated_tests_with_java_erudio/StartupTests.java
@@ -0,0 +1,14 @@
+package br.com.erudio.automated_tests_with_java_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
