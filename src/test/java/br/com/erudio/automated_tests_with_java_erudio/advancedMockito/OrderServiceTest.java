c119438eb52a5f0d3e3df60be6f1310229c4f19c
joao_junio <joaojunio818@gmail.com>
2026-02-11 18:47:18 -0300

    Mockando um LocalDatetime

--- src/test/java/br/com/erudio/automated_tests_with_java_erudio/advancedMockito/OrderServiceTest.java
+++ src/test/java/br/com/erudio/automated_tests_with_java_erudio/advancedMockito/OrderServiceTest.java
@@ -1,19 +1,22 @@
 package br.com.erudio.automated_tests_with_java_erudio.advancedMockito;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderServiceTest {
 
     private final OrderService service = new OrderService();
     private final UUID defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+    private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2026, 11, 2, 18, 41);
 
-    @DisplayName("Should Include Randon Order Id When No Order Id Exists")
+    @DisplayName("Should include randon OrderId when no OrderId exists")
     @Test
     void testShouldIncludeRandonOrderId_When_NoOrderIdExists() {
         // Given / Arrange
@@ -25,6 +28,21 @@ public class OrderServiceTest {
 
             // Then / Assert
             assertEquals(defaultUuid.toString(), result.getId());
+        }
+    }
+
+    @DisplayName("Should nnclude current time create a new Order")
+    @Test
+    void testShouldIncludeCurrentTime_When_CreateANewOrder() {
+        // Given / Arrange
+        try (MockedStatic<LocalDateTime> mockedUuid = Mockito.mockStatic(LocalDateTime.class)) {
+            mockedUuid.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
+
+            // When / Act
+            Order result = service.createOrder("MacBook Pro", 2L, null);
+
+            // Then / Assert
+            assertEquals(defaultLocalDateTime, result.getCreationDate());
         }
     }
 }
